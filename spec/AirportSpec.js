e75b00b3c7a45626512ebb4297c01beb17693d46
Merve Silk <merve.silk@gmail.com>
2015-06-12 23:31:42 +0100

    airport spec started

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,1 +1,27 @@
+describe("Airport", function() {
 
+  var airport;
+  var plane;
+
+  // beforeEach(function() {
+  //   // plane = jasmine.createSpyObj([]);
+  // )};
+
+  describe("by default ", function() {
+    it("starts with all planes DOCKED", function() {
+      airport = new Airport();
+      expect(airport.maximumCapacity).toEqual(true);
+    });
+
+    xit("can hold a maximum of 10 planes", function() {
+      airport = new Airport();
+      plane.bringPLaneIntoLand(10);
+
+    });
+  });
+
+
+
+
+
+});
