d2e3ba4640dd0dfc607592c7a67ff4ecacca3e92
Merve Silk <merve.silk@gmail.com>
2015-06-17 23:50:21 +0100

    removed toggle method for plane, plane rewrite begun, first two tests passing

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -5,36 +5,16 @@ describe("Plane", function() {
       plane = new Plane();
     });
 
-  describe("by default ", function() {
-    it("upon creation, is FLYING", function() {
-      expect(plane.isThePlaneInFlight).toBe(true);
+  describe("by default, ", function() {
+    it("has a status of FLYING when created", function() {
+      expect(plane.isFlying).toBe(true);
     });
   });
 
-  describe("can change its mobility status and go from ", function() {
-    it("FLYING to DOCKED in Airport", function() {
-      plane.togglePlaneFlyingStatus();
-      expect(plane.isThePlaneInFlight).toBe(false);
-    });
-
-    it("or DOCKED in Airport to FLYING", function() {
-      plane.togglePlaneFlyingStatus();
-      expect(plane.isThePlaneInFlight).toBe(false);
-      plane.togglePlaneFlyingStatus();
-      expect(plane.isThePlaneInFlight).toBe(true);
-    });
-  });
-
-  describe("cannot ", function() {
-    it("change its status to FLYING if it is already in the air", function() {
-      plane.takeOff();
-      expect(function() { plane.takeOff(); }).toThrow(new Error("You cannot Take off, you are already Flying!"));
-    });
-
-    it("change its status to DOCKED if it is already on the ground", function() {
-      plane.togglePlaneFlyingStatus();
-      plane.landPlane();
-      expect(function() { plane.landPlane(); }).toThrow(new Error("You cannot Land, you are already Docked!"));
+  describe("can change its status from ", function() {
+    it("FLYING to LAND", function() {
+      plane.land();
+      expect(plane.isFlying).toBe(false);
     });
   });
 
--- src/Plane.js
+++ src/Plane.js
@@ -1,28 +1,11 @@
 var Plane = function() {
-  this.isThePlaneInFlight = true;
+  this.isFlying = true;
 };
 
-Plane.prototype.togglePlaneFlyingStatus = function() {
-  if (this.isThePlaneInFlight === true) {
-    this.isThePlaneInFlight = false;
+Plane.prototype.land = function() {
+  if (this.isFlying === true) {
+    this.isFlying = false;
   } else {
-    this.isThePlaneInFlight = true;
-  }
-};
-
-Plane.prototype.takeOff = function() {
-  if (this.isThePlaneInFlight === true) {
-    throw new Error("You cannot Take off, you are already flying!");
-  } else {
-    this.isThePlaneInFlight = true;
-  }
-};
-
-
-Plane.prototype.landPlane = function() {
-  if (this.isThePlaneInFlight === false) {
-    throw new Error("You cannot Land, you are already Docked!");
-  } else {
-    this.isThePlaneInFlight = true;
+    this.isFlying = true;
   }
 };
